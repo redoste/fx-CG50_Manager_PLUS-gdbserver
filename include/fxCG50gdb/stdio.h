3301000e9609d38c05ec4a98783b439079cd6969
redoste <redoste@redoste.xyz>
2020-12-19 22:48:18 +0100

    Moved our custom printf to its own unit since it can't be inlined anyway

--- include/fxCG50gdb/stdio.h
+++ include/fxCG50gdb/stdio.h
@@ -1,15 +1,6 @@
 #ifndef STDIO_H
 #define STDIO_H
 
-#include <stdarg.h>
-#include <stdio.h>
-
-static inline void fxCG50gdb_printf(const char* format, ...) {
-	va_list args;
-	va_start(args, format);
-	printf("[fxCG50gdb] ");
-	vprintf(format, args);
-	va_end(args);
-}
+void fxCG50gdb_printf(const char* format, ...);
 
 #endif
--- src/gdb.c
+++ src/gdb.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <winsock2.h>
